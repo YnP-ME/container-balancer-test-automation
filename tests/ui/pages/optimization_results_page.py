56ceab95bfd1badc5b36d7945a83fc62a7d9840e
Milena Mkrtchian <milenamkrtchian@Milenas-MacBook-Pro.local>
2026-02-02 17:26:12 +0100

    Added Port Setup page and test and Optimization Result page

---
+++ tests/ui/pages/optimization_results_page.py
@@ -0,0 +1,18 @@
+from playwright.async_api import expect
+from tests.ui.pages.base_page import BasePage
+
+class Optimization_Result_Page(BasePage):
+    """Page object model for the Optimization Result page."""
+    def __init__(self, page, base_url):
+        super().__init__(page)
+        self.base_url = base_url
+
+        # ---------- Optimization Result  page elements ----------
+        self.optimization_result_section = page.get_by_text("Optimization Result (12 weeks)")
+        self.optimization_result_table_container = page.locator("div.min-h-0.flex-1.overflow-y-auto")
+
+    def is_page_opened(self):
+        """Fail test if page is not loaded."""
+        expect( self.optimization_result_section,"Optimization Result header is not visible").to_be_visible()
+        expect(self.optimization_result_table_container,"Optimization Result table container is not visible").to_be_visible()
+
