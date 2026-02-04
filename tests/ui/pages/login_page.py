7c70949ae76c03397e3a238904778de66061708c
Milena Mkrtchian <milenamkrtchian@Milenas-MacBook-Pro.local>
2026-02-04 15:15:28 +0100

    added new check

--- tests/ui/pages/login_page.py
+++ tests/ui/pages/login_page.py
@@ -1,5 +1,4 @@
-import time
-
+from playwright.sync_api import expect
 from tests.ui.pages.base_page import BasePage
 
 class LoginPage(BasePage):
@@ -25,10 +24,12 @@ class LoginPage(BasePage):
 
     def enter_username(self, username):
         """Fill in the username field."""
+        expect(self.username_input).to_be_visible(timeout=30000)
         self.username_input.fill(username)
 
     def enter_password(self, password):
         """Fill in the password field."""
+        expect(self.password_input).to_be_visible(timeout=30000)
         self.password_input.fill(password)
 
     def click_login(self):
