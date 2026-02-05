eaa18b54e1d7a26cb54474985ee8e309c6a2d930
Milena Mkrtchian <milenamkrtchian@Milenas-MacBook-Pro.local>
2026-02-05 13:16:42 +0100

    fixing CI error

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,12 @@ import yaml
 import pytest
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright, expect
+
 from ui.pages.login_page import LoginPage
 from ui.pages.base_page import BasePage
 
+
+# ---------- ENV ----------
 load_dotenv()
 
 USERNAME = os.getenv("USERNAME")
@@ -40,6 +43,7 @@ def pytest_addoption(parser):
 
 
 # ---------- Fixtures ----------
+
 @pytest.fixture(scope="session")
 def config():
     return load_config()
@@ -52,16 +56,26 @@ def base_url(request, config):
         or os.getenv("TEST_ENV")
         or config.get("default_env", "staging")
     )
+
     url = config["environments"][active_env]
     print(f"\nRunning tests against: {active_env} → {url}")
     return url
 
 
+@pytest.fixture(scope="session")
+def playwright_instance():
+    """Start Playwright once per test session (Allure-safe)"""
+    with sync_playwright() as p:
+        yield p
+
+
 @pytest.fixture
-def browser_page():
-    """Start a fresh browser per test (sync). Works without async."""
-    playwright = sync_playwright().start()
-    browser = playwright.chromium.launch(headless=False)
+def browser_page(playwright_instance, request):
+    """Fresh browser per test"""
+
+    browser_name = request.config.getoption("--browser")
+
+    browser = getattr(playwright_instance, browser_name).launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
@@ -69,12 +83,12 @@ def browser_page():
 
     context.close()
     browser.close()
-    playwright.stop()
 
 
 @pytest.fixture
 def login(browser_page, base_url):
-    """Login fixture for tests (sync)"""
+    """Login before test"""
+
     base = BasePage(browser_page)
     login_page = LoginPage(browser_page, base_url)
 
@@ -85,4 +99,5 @@ def login(browser_page, base_url):
     login_page.click_login()
 
     expect(base.logout_button).to_be_visible(timeout=10000)
+
     return browser_page
